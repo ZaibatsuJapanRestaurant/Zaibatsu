258adaf83d8e73efaa0287e643e92d180d574655
ZaibatsuJapanRestaurant <xmrkaybi@gmail.com>
2025-03-05 09:00:34 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-// scripts.js
 document.addEventListener("DOMContentLoaded", function() {
     const audio = document.getElementById("background-music");
-    const playButton = document.getElementById("play-button");
-    const pauseButton = document.getElementById("pause-button");
+    const toggleButton = document.getElementById("toggle-button");
+    const playIcon = document.getElementById("play-icon");
 
     // Verificar si la música ya está en reproducción
     if (localStorage.getItem("musicPlaying") === "true") {
@@ -14,18 +13,22 @@ document.addEventListener("DOMContentLoaded", function() {
     // Almacenar el estado de la música
     audio.addEventListener("play", function() {
         localStorage.setItem("musicPlaying", "true");
+        playIcon.classList.remove("fa-play");
+        playIcon.classList.add("fa-pause");
     });
 
     audio.addEventListener("pause", function() {
         localStorage.setItem("musicPlaying", "false");
+        playIcon.classList.remove("fa-pause");
+        playIcon.classList.add("fa-play");
     });
 
     // Controlar la reproducción
-    playButton.addEventListener("click", function() {
-        audio.play();
-    });
-
-    pauseButton.addEventListener("click", function() {
-        audio.pause();
+    toggleButton.addEventListener("click", function() {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
     });
 });
